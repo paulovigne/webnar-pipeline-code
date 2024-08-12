bd997b0d8da80eeb940f90c107b3ab30c787a5b3
paulovigne <57726813+paulovigne@users.noreply.github.com>
2024-08-12 12:01:03 -0300

    Update server.go

--- server.go
+++ server.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "<h1>Pipeline All in One!</h1>")
+    fmt.Fprintf(w, "<h1>Pipeline All in One!!!</h1>")
 }
 
 func main() {
