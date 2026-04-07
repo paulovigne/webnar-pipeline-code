4ab915eaca8619f927eadf289033d80ef79d81fa
paulovigne <57726813+paulovigne@users.noreply.github.com>
2026-04-06 21:39:26 -0300

    Update helloWorld response message

--- server.go
+++ server.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "<h1>Pipeline 2026</h1>")
+    fmt.Fprintf(w, "<h1>Pipeline 2026 ArgoCD Openshift</h1>")
 }
 
 func main() {
